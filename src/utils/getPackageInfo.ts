e2ae233807a0203055a14102cc833c6277d49ac1
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-09-27 23:28:13 +0700

    update import package.json

--- src/utils/getPackageInfo.ts
+++ src/utils/getPackageInfo.ts
@@ -1,8 +1,5 @@
-import path from 'path'
-import fs from 'fs-extra'
-import { type PackageJson } from 'type-fest'
+import PackageJson from '../../package.json'
 
 export const getPackageInfo = async () => {
-  const packageJsonPath = path.join('package.json')
-  return fs.readJSONSync(packageJsonPath) as PackageJson
+  return PackageJson
 }
